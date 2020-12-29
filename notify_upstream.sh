167b4c1992294f12de0d55c4c0c4b17e681124fe
Vitalii Motso <vitalii.motso@gmail.com>
2020-12-29 22:09:09 +0200

    update 12

---
+++ notify_upstream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Create status for commit"
+echo '{ "state": "'$1'", "context": "downstream/provider", "description": "Check provider", "target_url": "https://github.com/vmotso-labs/like_provider/actions/runs/'$GITHUB_RUN_ID'" }'
+
+curl -u admin:$ORG_ADMIN_TOKEN \
+  -X POST '{ "state": "'$1'", "context": "downstream/provider", "description": "Check provider", "target_url": "https://github.com/vmotso-labs/like_provider/actions/runs/'$GITHUB_RUN_ID'" }' \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/vmotso-labs/like_fatmouse/statuses/$UPSTREAM_SHA
